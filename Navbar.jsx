9411e19c341dc56264e8e381b88ebb9e9edc202a
adaarellano <147354454+adaarellano@users.noreply.github.com>
2025-03-17 19:22:33 -0400

    Update Navbar.jsx

--- Navbar.jsx
+++ Navbar.jsx
@@ -16,11 +16,12 @@ export default function Navbar(){
             
             <ul className="flex bg-amber-700 justify-around text-4xl text-nowrap text-white font-bold">
                 <img src="imagenes/Metrologo.png" className="w-33 h-33 hover:opacity-65" onClick={mandarLink}></img>
-                    <Link to='/home' class='hover:bg-orange-400 py-12 px-10'>Inicio</Link>
-                    <Link to='/sabasNieves' class='hover:bg-orange-400 py-12 px-10'>Rutas</Link>
-                    <Link to='/Contacto'className="flex gap-10 py-12 hover:bg-orange-400">Contacto</Link>
-                    <Link to='/login' className="flex gap-10 py-12 hover:bg-orange-400">Iniciar sesion</Link>
-                    <Link to='/Register'className="flex gap-10 py-12 hover:bg-orange-400">Registrarme</Link>
+                    <Link to='/home' class='hover:bg-orange-400 py-12 px-8'>Inicio</Link>
+                    <Link to='/sabasNieves' class='hover:bg-orange-400 py-12 px-8'>Rutas</Link>
+                    <Link to='/Contacto'className="flex gap-20 py-12 hover:bg-orange-400">Contacto</Link>
+                    <Link to='/Foro'className="flex gap-5 py-12 hover:bg-orange-400">Foro</Link>
+                    <Link to='/login' className="flex gap-5 py-12 hover:bg-orange-400">Iniciar sesion</Link>
+                    <Link to='/Register'className="flex gap-5 py-12 hover:bg-orange-400">Registrarme</Link>
                     
             </ul>
         </nav>
