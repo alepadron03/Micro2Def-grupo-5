eeaab3a4ec22bc88212a8edfaa0302a090e8e127
adaarellano <147354454+adaarellano@users.noreply.github.com>
2025-03-17 19:21:52 -0400

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -4,8 +4,11 @@ import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import NotFound from "./pages/NotFound"
-import CONTACTO from './pages/Contacto'
 import Contacto from './pages/Contacto'
+import PiedraIndio from './pages/PiedraIndio'
+import PicoNa from './pages/PicoNa'
+import CruzP from './pages/CruzP'
+import Foro from './pages/Foro'
 
 export default function App(){
   return(
@@ -15,8 +18,12 @@ export default function App(){
       <Route path='home' element={<Home />} />
       <Route path='login' element={<Login />} />
       <Route path='Contacto' element={<Contacto />} />
+      <Route path='Foro' element={<Foro />} />
       <Route path='register' element={<Register />} />
       <Route path='SabasNieves' element={<NotFound />} />
+      <Route path='PiedraIndio' element={<PiedraIndio />} />
+      <Route path='PicoNa' element={<PicoNa />} />
+      <Route path='CruzP' element={<CruzP />} />
       <Route path='*' element={<Home />} />
 
 
@@ -33,3 +40,4 @@ export default function App(){
 
   )
 }
+
