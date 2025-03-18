643302c61983351f7996e0580b90c469f5232cd4
AlePadron <122551361+alepadron03@users.noreply.github.com>
2025-03-17 22:58:56 -0300

    Mejoras al Footer y botones adicionales en el navbar
    -Se agrego un footer alternativo que sigue el diseno del navbar. No se desecho el footer anterior hasta no hablar con el equipo.
    -Se agregaron mas links de navegacion en el navbar.
    -Se modificaron los estilos del navbar y footer a traves de los estilos en el archivo .css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,48 @@
 import React from "react";
 import { Link } from "react-router";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-
-
-export default function Footer(){
+export default function Footer() {
 
     const volverArriba = () => {
-        window.scrollTo({top:0,left:0,behavior:'smooth'})
+        window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
     }
 
-    return(
+    return (
+        < div className="custom-navbar">
+            <Nav className="justify-content-center" activeKey="/home">
+                <Nav.Item>
+                    <Nav.Link href="/home">Inicio</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="Foro">Reviews</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="Contacto">Contacto</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="disabled" disabled>
+                        Disabled
+                    </Nav.Link>
+                </Nav.Item>
+            </Nav>
+            <Navbar.Brand className="d-flex justify-content-center w-100" href="/home">
+                <img
+                    src="http://www.unimet.edu.ve/wp-content/uploads/2023/07/Logo-footer.png"
+                    width="150"
+                    height="90"
+                    alt="img"
+                />
+            </Navbar.Brand>
+            <nav className='mt-30'>
 
-        <nav className='mt-30'>
-            
-            <ul className="flex bg-amber-800 justify-around text-4xl text-nowrap text-white font-bold ">
-                
-                <button onClick={volverArriba} class='hover:bg-amber-700 py-10'>Inicio de la pagina</button>
+                <ul className="flex bg-amber-800 justify-around text-4xl text-nowrap text-white font-bold ">
 
-            </ul>
-        </nav>
-        
+                    <button onClick={volverArriba} class='hover:bg-amber-700 py-10'>Regresar Al Tope de la pagina</button>
+
+                </ul>
+            </nav>
+        </div>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import Navigator from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from "react-router";
-
+import '../index.css';
 
 
 export default function Header() {
@@ -20,60 +20,68 @@ export default function Header() {
     return (
 
         <div>
-            <Navbar expand="lg" className="bg-body-tertiary">
-                <Container fluid>
-                    <Navbar.Brand href="/home">
-                        <img
-                            src="imagenes/Metrologo.png"
-                            width="40"
-                            height="40"
-                            className="d-inline-block align-top"
-                            alt="React Bootstrap logo"
-                        />
-                    </Navbar.Brand>
+            <Navbar expand="lg" className="custom-navbar">
+  <Container fluid>
+    <Navbar.Brand href="/home">
+      <img
+        src="imagenes/Metrologo.png"
+        width="40"
+        height="40"
+        className="d-inline-block align-top"
+        alt="img"
+      />
+    </Navbar.Brand>
 
-                    <Navbar.Toggle aria-controls="navbarScroll" />
-                    <Navbar.Collapse id="navbarScroll">
-                        <Nav
-                            className="me-auto my-2 my-lg-0"
-                            style={{ maxHeight: '100px' }}
-                            navbarScroll
-                        >
-                            <Nav.Link href="/home">Inicio</Nav.Link>
-                            <Nav.Link href="/sabasNieves">Rutas</Nav.Link>
-                            <Nav.Link href="/home">Guías</Nav.Link>
+    <Navbar.Toggle aria-controls="navbarScroll" />
+    <Navbar.Collapse id="navbarScroll">
+      <Nav
+        className="me-auto my-2 my-lg-0"
+        style={{ maxHeight: "100px" }}
+        navbarScroll
+      >
+        <Nav.Link href="/home">Inicio</Nav.Link>
+        <Nav.Link href="/sabasNieves">Rutas</Nav.Link>
+        <Nav.Link href="/home">Guías</Nav.Link>
+        <Nav.Link href="/Foro">Reviews</Nav.Link>
+        <Nav.Link href="/Contacto">Contacto</Nav.Link>
 
-                            <Form className="d-flex">
-                                <Form.Control
-                                    type="search"
-                                    placeholder="Search"
-                                    className="me-2"
-                                    aria-label="Search"
-                                />
-                                <Button variant="outline-success">Search</Button>
-                            </Form>
-                        </Nav>
+        <Form className="d-flex">
+          <Form.Control
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
+          <Button variant="outline-light">Search</Button> {/* Botón con variante clara */}
+        </Form>
+      </Nav>
 
-                        <Nav className="ms-auto"
-                            style={{ maxHeight: '100px' }}
-                            navbarScroll>
-                            <Nav.Link href="/Register">Registrarte</Nav.Link>
-                            <Nav.Link href="/login">Iniciar Sesión</Nav.Link>
-                        </Nav>
-
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-            <nav className=''>
-
-                <ul className="flex bg-blue-800 justify-around text-4xl text-nowrap text-white font-bold">
-                    <img src="imagenes/Metrologo.png" className="w-33 h-33 hover:opacity-65" onClick={mandarLink}></img>
-                    <Link to='/home' class='hover:bg-blue-700 py-12 px-10'>INICIO</Link>
-                    <Link to='/sabasNieves' class='hover:bg-blue-700 py-12 px-10'>RUTAS</Link>
-                    <Link to='/login' className="flex gap-10 py-12 hover:bg-blue-700">INICIAR SESION</Link>
-                    <Link to='/Register' className="flex gap-10 py-12 hover:bg-blue-700">REGISTRAR</Link>
-                </ul>
-            </nav>
+      <Nav
+        className="ms-auto"
+        style={{ maxHeight: "100px" }}
+        navbarScroll
+      >
+        <Nav.Link href="/Register">Registrarte</Nav.Link>
+        <Nav.Link href="/login">Iniciar Sesión</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+           {/*} 
+           <nav className=''>
+            
+            <ul className="flex bg-amber-700 justify-around text-4xl text-nowrap text-white font-bold">
+                <img src="imagenes/Metrologo.png" className="w-33 h-33 hover:opacity-65" onClick={mandarLink}></img>
+                    <Link to='/home' class='hover:bg-orange-400 py-12 px-8'>Inicio</Link>
+                    <Link to='/SabasNieves' class='hover:bg-orange-400 py-12 px-8'>Rutas</Link>
+                    <Link to='/Contacto'className="flex gap-20 py-12 hover:bg-orange-400">Contacto</Link>
+                    <Link to='/Foro'className="flex gap-5 py-12 hover:bg-orange-400">Foro</Link>
+                    <Link to='/login' className="flex gap-5 py-12 hover:bg-orange-400">Iniciar sesion</Link>
+                    <Link to='/Register'className="flex gap-5 py-12 hover:bg-orange-400">Registrarme</Link>
+                    
+            </ul>
+        </nav>
+        {*/} 
         </div>
 
     )
